20b608e8ebe0ec6db78ad51b5762b6defb6fad54
Jackson <upjw0429@gmail.com>
2023-11-02 17:50:48 +0900

    Add. Whisper 음성인식 텍스트 반환하는 API 구현 완료

--- whisper/whisper_server/api/views.py
+++ whisper/whisper_server/api/views.py
@@ -15,13 +15,16 @@ def getText(request):
         return HttpResponse(status=400)
 
     file = request.FILES['audio']
-    fs = FileSystemStorage(location="audio/")
-    saved_file = fs.save(file.name, file)
+    fs = FileSystemStorage(location="audio/") # audio 디렉토리에 저장
+    saved_file = fs.save(file.name, file) # 해당 파일 이름으로 위에서 지정한 audio/ 디렉토리에 파일 저장
 
     print("Request", request)
     print("Request", request.FILES['audio'])
 
-    model = whisper.load_model("base")
-    result = model.transcribe("audio/" + str(saved_file), fp16=False)
+    model = whisper.load_model("base") # base Model 사용
+    result = model.transcribe("audio/" + str(saved_file), fp16=False, language="ko")
+    # fp16=False 옵션 적용하지 않으면 경고 발생 -> warnings.warn("FP16 is not supported on CPU; using FP32 instead")
+    # 옵션으로 언어 지정 가능
+    print(result["text"])
 
     return Response(result["text"])
